8b108006cf9d1b223db8519930928dee9b0f7b4d
MrTompkinsGamow <mhbrandenburgh@gmail.com>
2023-01-21 16:23:17 -0500

    added five day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
 var searchButtonEl = $("#go");
 var cityEl = $('#city-search');
+var fiveDayEl = $('#fiveDay');
 //var locationUrl = 'http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid=25aec6e1a5f4bd4d4c5e2b4868c2e0e3'
-var requestUrl = 'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid=25aec6e1a5f4bd4d4c5e2b4868c2e0e3'
+//var requestUrl = 'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid=25aec6e1a5f4bd4d4c5e2b4868c2e0e3'
 
 $(searchButtonEl).on("click", getLocation);
 
@@ -32,5 +33,17 @@ function getWeather(coords) {
         $('#cityTemp').text(`${data.list[0].main.temp}`);
         $('#cityWind').text(`${data.list[0].wind.speed}`);
         $('#cityHumidity').text(`${data.list[0].main.humidity}`);
+        for (var i = 8; i<41; i+=8) {
+            var newCard = $('<div>');
+            newCard.addClass('col');
+            var dayInfo = data.list[i];
+            newCard.html(`
+            <h3>${dayjs.unix(dayInfo.dt).format('M/D')}</h3>
+            <p>Temp: ${dayInfo.main.temp}°F</p>
+            <p>Wind: ${dayInfo.wind.speed}</p>
+            <p>Humidity: ${dayInfo.main.humidity}%</p>
+            `);
+            fiveDayEl.append(newCard);
+        }
     })
 }
